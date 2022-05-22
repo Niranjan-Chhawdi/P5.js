b327594c51a1d6550cdda8405dbb7e398bf5277f
Niranjan Chhawdi <64586036+Niranjan-Chhawdi@users.noreply.github.com>
2022-05-22 22:20:43 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# P5.js
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ app.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(400,400);
+  createSprite(100, 200, 50, 50);
+}
+
+function draw() {
+  background(0,0,0);  
+  drawSprites();
+}
